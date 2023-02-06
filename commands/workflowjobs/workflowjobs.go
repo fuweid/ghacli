a10a63c13fadd189866a64f3ba1d099736fc5782
Wei Fu <fuweid89@gmail.com>
2023-02-06 23:21:36 +0800

    commands/workflowjobs: add subcommand for list jobs for a run
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- commands/workflowjobs/workflowjobs.go
+++ commands/workflowjobs/workflowjobs.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"text/tabwriter"
 
 	"github.com/urfave/cli"
 
@@ -17,6 +18,7 @@ var Command = cli.Command{
 	Usage: "About workflow jobs for a repository in GitHub Actions",
 	Subcommands: []cli.Command{
 		downloadLogCommand,
+		listJobsCommand,
 	},
 }
 
@@ -54,5 +56,57 @@ var downloadLogCommand = cli.Command{
 			return fmt.Errorf("failed to read job %v log: %w", jobID, err)
 		}
 		return nil
+	},
+}
+
+var listJobsCommand = cli.Command{
+	Name:  "list",
+	Usage: "List workflow jobs with a given workflow run ID",
+	Action: func(cliCtx *cli.Context) error {
+		if !cliCtx.Args().Present() {
+			return fmt.Errorf("run ID is required")
+		}
+		if cliCtx.NArg() > 1 {
+			return fmt.Errorf("only support one run")
+		}
+
+		runID, err := strconv.ParseInt(cliCtx.Args().First(), 10, 64)
+		if err != nil {
+			return fmt.Errorf("failed to parse %s into int64: %w",
+				cliCtx.Args().First(), err)
+		}
+
+		ctx, client, err := commands.NewGithubClient(cliCtx)
+		if err != nil {
+			return fmt.Errorf("failed to init github client: %w", err)
+		}
+		owner, repo := commands.TargetRepo(cliCtx)
+
+		jobs, err := commands.ListAllJobsForARun(ctx, client.Actions, owner, repo, runID)
+		if err != nil {
+			return fmt.Errorf("failed to list jobs for run %v: %w", runID, err)
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 4, 8, 2, ' ', 0)
+
+		header := "ID\tNAME\tSTATUS\tRUN_ATTEMPT\tWORKFLOW_NAME"
+		fmt.Fprintln(w, header)
+		for _, job := range jobs {
+			status := *job.Status
+			if job.Conclusion != nil {
+				status = *job.Conclusion
+			}
+			_, err := fmt.Fprintf(w, "%d\t%s\t%s\t%d\t%s\n",
+				*job.ID,
+				*job.Name,
+				status,
+				*job.RunAttempt,
+				*job.WorkflowName,
+			)
+			if err != nil {
+				return fmt.Errorf("failed to print result: %w", err)
+			}
+		}
+		return w.Flush()
 	},
 }
